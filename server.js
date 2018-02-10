354da03400219d35eb3b15b75167c734902e49fb
user <user@users-MacBook-Pro.local>
2018-02-10 15:47:24 -0800

    added newUser functionality

--- server.js
+++ server.js
@@ -9,10 +9,28 @@ app.use(express.static(path.join(__dirname, './static')));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
+var activeUsers = [];
+var messages = [];
+
 app.get('/', function(req, res) {
-    res.send('hola');
+    res.render('index');
 })
 
-app.listen(8000, function() {
+var server = app.listen(8000, function() {
     console.log('listening on 8000');
 });
+
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function(socket) {
+    console.log('Socket connection made');
+    console.log('Client socket ID: ', socket.id);
+    socket.on('new_user_connected', function(data) {
+        var newUser = 
+        {name: data.name,
+        id: socket.id,
+        };
+        conosle.log("Welcome: ", newUser);
+        activeUsers.append(newUser);
+        socket.emit('user_id', {id: socket.id});
+    })
+})
