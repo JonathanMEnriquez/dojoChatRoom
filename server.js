5f415b0576c311791191c835681672891a97d25b
user <user@users-MacBook-Pro.local>
2018-02-10 18:37:31 -0800

    mostly functional. would like to add some features.

--- server.js
+++ server.js
@@ -24,13 +24,21 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket) {
     console.log('Socket connection made');
     console.log('Client socket ID: ', socket.id);
+
     socket.on('new_user_connected', function(data) {
         var newUser = 
-        {name: data.name,
-        id: socket.id,
-        };
-        conosle.log("Welcome: ", newUser);
-        activeUsers.append(newUser);
+            {name: data.name,
+            id: socket.id,};
+        console.log("Welcome: ", newUser);
+        activeUsers.push(newUser);
         socket.emit('user_id', {id: socket.id});
+        // emit all of the message objects
+        socket.emit('allMessages', {messages: messages});
+    })
+
+    socket.on('newMessage', function(data) {
+        var newMessage = data.message;
+        messages.push(data.message);
+        io.emit('messageAdded', {message: newMessage});
     })
 })
